c888aac1b41278d1e17809bda323c63b1bcd8282
Robert Owens <robertowens95@gmail.com>
2023-11-15 15:03:44 -0600

    added accordion

--- js/index.js
+++ js/index.js
@@ -1,39 +1,58 @@
-const coffees = [
-    { id: 1, name: "Espresso", roastType: "Dark" },
-    { id: 2, name: "Americano", roastType: "Medium" },
-    { id: 3, name: "Cappuccino", roastType: "Medium" },
-    { id: 4, name: "Latte", roastType: "Light" },
-    { id: 5, name: "Mocha", roastType: "Dark" },
-    { id: 6, name: "Macchiato", roastType: "Medium" },
-    { id: 7, name: "Flat White", roastType: "Light" },
-    { id: 8, name: "Affogato", roastType: "Dark" },
-    { id: 9, name: "Irish Coffee", roastType: "Dark" },
-    { id: 10, name: "Turkish Coffee", roastType: "Dark" },
-    { id: 11, name: "Pour Over", roastType: "Light" },
-    { id: 12, name: "French Press", roastType: "Medium" },
-    { id: 13, name: "Vietnamese Iced Coffee", roastType: "Dark" },
-    { id: 14, name: "Cold Brew", roastType: "Dark" },
-];
+// const toggleList = () => {
+//     const accordionDiv = document.querySelectorAll(".accordion");
+//     const accordionItem = document.querySelectorAll(".accordion ul");
+//     for (let item of accordionItem) {
+//         const itemId = item.getAttribute("[data-list]");
+//         item.classList.add("hidden");
+//     }
+//     for (let div of accordionDiv) {
+//         div.addEventListener("click", (e) => {
+//             const accordionItem = div.querySelector("ul");
+//             accordionItem.classList.add("display");
+//             accordionItem.classList.remove("hidden");
+//             const accordionBtn = div.querySelector(".icon-button");
+//             accordionBtn.addEventListener("click", (e) => {});
+//         });
+//     }
+// };
 
-const renderCoffeeElement = (coffee) => {
-    let tr = document.querySelector(".tr").createElement("tr");
-    tr.classList.add("coffee");
+const toggleList = () => {
+    const accordionDivs = document.querySelectorAll(".accordion");
+    accordionDivs.forEach((div) => {
+        const accordionItem = div.querySelector("ul");
+        const accordionBtn = div.querySelector(".icon-btn");
+        const removeBtn = div.querySelector(".remove-btn");
+        const addBtn = div.querySelector(".add-btn");
+        removeBtn.classList.add("hidden");
+        addBtn.classList.add("display");
+        accordionItem.classList.add("hidden"); // Initially hide all accordion items
 
-    tr.innerHTML = `
-        <td>${coffee.id}</td>
-        <td>${coffee.name}</td>
-        <td>${coffee.roastType}</td>
-    `;
-    return tr;
+        accordionBtn.addEventListener("click", (e) => {
+            e.stopPropagation(); // Prevent unnecessary event bubbling
+            // Toggle visibility of accordion item
+            if (accordionItem.classList.contains("hidden")) {
+                // If hidden, show the item
+                accordionItem.classList.remove("hidden");
+                accordionItem.classList.add("display");
+                //
+                removeBtn.classList.remove("hidden");
+                addBtn.classList.add("hidden");
+            } else {
+                // If displayed, hide the item
+                accordionItem.classList.add("hidden");
+                accordionItem.classList.remove("display");
+                //
+                removeBtn.classList.remove("display");
+                removeBtn.classList.add("hidden");
+                //
+                addBtn.classList.remove("hidden");
+                addBtn.classList.add("display");
+            }
+        });
+    });
 };
-function toggleList(element) {
-    // Toggle the visibility of the nested ul element
-    const nestedList = element.querySelector("ul");
-    nestedList.style.display =
-        window.getComputedStyle(nestedList).display === "none"
-            ? "block"
-            : "none";
-}
 
 //MAIN
-(() => {})();
+(() => {
+    toggleList();
+})();
