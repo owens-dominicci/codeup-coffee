c76a715ed0d11583f4c617523121f8b3683e71f9
Robert Owens <robertowens95@gmail.com>
2023-11-15 12:29:45 -0600

    added tabbing capes

--- js/tabs.js
+++ js/tabs.js
@@ -1,30 +1,44 @@
-const openTab = () => {
-    const tabContent = document.querySelectorAll(".tab-content");
-    console.log(tabContent);
+const openTab = (tabId = "create-coffee") => {
+    //get the tablinks
     const tabLinks = document.querySelectorAll(".tablink");
-    const defaultTab = document.querySelector(".default");
-    console.log(tabLinks);
-    tabContent.forEach((tab) => {
-        if (!tab.classList.contains("default")) {
-            tab.classList.add("hidden");
+    //looping through the tablinks to get the ID and add the active class to the tablink
+    for (let tabLink of tabLinks) {
+        if (tabLink.getAttribute("data-tab") === tabId) {
+            tabLink.classList.add("active");
+        } else {
+            tabLink.classList.remove("active");
         }
-    });
-    tabLinks.forEach((tab) => {
-        if (!tab.classList.contains("default")) {
-            tab.classList.remove("active");
-        }
-        if (tab.classList.contains("active")) {
-            tab.classList.remove("active");
-        }
-    });
+    }
+    //targeting the tab-content
+    const tabContents = document.querySelectorAll(".tab-content");
+    //looping  through the tab content and adding a default of hidden, and active if it's not supposed to be shown
+    for (let tabContent of tabContents) {
+        tabContent.classList.add("hidden");
+        tabContent.classList.remove("active");
+    }
+    //targeting the tab-content with the data-content attribute that matches the tabId
+    const tabContent = document.querySelector(
+        `.tab-content[data-content="${tabId}"]`
+    );
+    //removing the hidden class and adding the active class
+    tabContent.classList.remove("hidden");
+    tabContent.classList.add("active");
+};
 
-    for (tabs of tabLinks) {
-        tabs.addEventListener("click", (e) => {
-            e.target.classList.add("active");
+const registerTabEvents = () => {
+    //get the tablinks
+    const tabLinks = document.querySelectorAll(".tablink");
+    //looping through the tablinks adding the click event listener, to get the tab ID.
+    for (let tabLink of tabLinks) {
+        tabLink.addEventListener("click", (e) => {
+            //get tab id
+            const tabId = e.target.getAttribute("data-tab");
+            openTab(tabId);
         });
     }
 };
 
 (() => {
     openTab();
+    registerTabEvents();
 })();
