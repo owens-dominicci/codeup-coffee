bbcd55808986d971fc2881a138b58f3cdb647917
Robert Owens <robertowens95@gmail.com>
2023-11-16 15:13:20 -0600

    rob-changes

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ const toggleList = () => {
 const options = {
     roast: ["light", "medium", "dark"],
     milk: ["whole", "2%", "skim", "almond", "oat", "soy"],
-    sweetener: ["sugar", "hazelnut", "coconut", "vanilla", "caramel"],
+    flavor: ["sugar", "hazelnut", "coconut", "vanilla", "caramel"],
     size: ["small", "medium", "large"],
 };
 
@@ -62,33 +62,32 @@ const createCoffee = () => {};
 const renderCoffee = () => {};
 const updateCoffee = () => {};
 
-const selectCoffee = (callback) => {
-    let userSelection = {
-        category: "option",
-    };
-    const roastOptions = document.querySelectorAll(
-        "form[data-select='selection']"
+const saveSelections = () => {
+    const formData = new FormData(
+        document.querySelector('form[name="options"]')
     );
-    console.log(roastOptions);
-    roastOptions.forEach((roast) => {
-        const userSel = roast.querySelectorAll("input[name='options']");
-        for (let selection of userSel) {
-            selection.addEventListener("change", (e) => {
-                // console.log(e.target.value);
-                userSelection = { roast: e.target.value };
-                if (callback && typeof callback === "function") {
-                    callback(userSelection); // Pass the updated selection to the callback
-                }
-            });
-        }
-    });
-    return userSelection;
+    const userSelections = {};
+
+    // Loop through the FormData entries and build the object
+    for (let [key, value] of formData.entries()) {
+        // Set the selections as sections: values (e.g., roast: light)
+        userSelections[key] = value;
+    }
+
+    return userSelections;
 };
+
+// Add event listener to the button
+const saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const selections = saveSelections();
+    console.log(selections); // Output the collected object with selected values
+    // You can further process or utilize selections as needed here
+});
 //MAIN
 (() => {
     toggleList();
-    selectCoffee((updatedSelection) => {
-        console.log(updatedSelection);
-    });
     populateSelect();
 })();
