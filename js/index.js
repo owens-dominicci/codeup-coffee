5b1c2b4cd059d86d03fec48cbeb95c76c105dbcd
raedownen <rae.l.downen@gmail.com>
2023-11-16 15:42:09 -0600

    coffee extract

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+const coffees = [
+	{ id: 1, name: "Espresso", roastType: "Dark" },
+	{ id: 2, name: "Americano", roastType: "Medium" },
+	{ id: 3, name: "Cappuccino", roastType: "Medium" },
+	{ id: 4, name: "Latte", roastType: "Light" },
+	{ id: 5, name: "Mocha", roastType: "Dark" },
+	{ id: 6, name: "Macchiato", roastType: "Medium" },
+	{ id: 7, name: "Flat White", roastType: "Light" },
+	{ id: 8, name: "Affogato", roastType: "Dark" },
+	{ id: 9, name: "Irish Coffee", roastType: "Dark" },
+	{ id: 10, name: "Turkish Coffee", roastType: "Dark" },
+	{ id: 11, name: "Pour Over", roastType: "Light" },
+	{ id: 12, name: "French Press", roastType: "Medium" },
+	{ id: 13, name: "Vietnamese Iced Coffee", roastType: "Dark" },
+	{ id: 14, name: "Cold Brew", roastType: "Dark" },
+];
 const toggleList = () => {
     const accordionDivs = document.querySelectorAll(".accordion");
 
@@ -86,7 +102,7 @@ saveButton.addEventListener("click", (event) => {
     console.log(selections); // Output the collected object with selected values
     // You can further process or utilize selections as needed here
 });
-export const extractCoffeeProperties = (coffees) => {
+const extractCoffeeProperties = (coffees) => {
 	return coffees.map(({ id, name, roastType }) => ({ id, name, roastType }));
 };
 //MAIN
